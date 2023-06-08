772c6262428a2ffb9c90678025dda2b725266af5
TeeMeeLehin <samemerald8@gmail.com>
2023-06-08 14:14:14 +0100

    python import modules

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    args = len(argv) - 1
+    index = 1
+    if (args == 0):
+        print("{} arguments.".format(args))
+    elif (args == 1):
+        print("{} argument:".format(args))
+        print("{}: {}".format(index, argv[index]))
+    else:
+        print("{} arguments:".format(args))
+        while (index <= args):
+            print("{}: {}".format(index, argv[index]))
+            index += 1
+
+            
+
