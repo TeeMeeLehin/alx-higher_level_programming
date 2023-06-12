93b29c9522afb7daf2adbc9c4c5c239c7bfa7d6b
TeeMeeLehin <samemerald8@gmail.com>
2023-06-12 11:38:40 +0100

    Python DSA Lists

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    l = len(sentence)
-    f = sentence[0] if l > 0 else None
-    res = (l, f)
+    length = len(sentence)
+    f = sentence[0] if length > 0 else None
+    res = (length, f)
     return res
