20358cb1b64af512d99d2d5fc1dd109741b469db
TeeMeeLehin <samemerald8@gmail.com>
2023-06-14 21:00:40 +0100

    roman to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    int_dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    rs = list(roman_string)
+    i = 0
+
+    while (i < len(rs)):
+        next_num = int_dic[rs[i + 1]] if i + 1 < len(rs) else 0
+        if int_dic[rs[i]] >= next_num:
+            result += int_dic[rs[i]]
+            i += 1
+        else:
+            result += (int_dic[rs[i + 1]] - int_dic[rs[i]])
+            i += 2
+    return result
