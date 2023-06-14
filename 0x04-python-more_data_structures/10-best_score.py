8a467fac5fa39ef243877916842716bff9497a34
TeeMeeLehin <samemerald8@gmail.com>
2023-06-14 19:19:26 +0100

    More Data Structures

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is not None:
+        scores = list(a_dictionary.values())
+        max = scores[0]
+        for i in range(len(scores) - 1):
+            max = max if max > scores[i + 1] else scores[i + 1]
+        for key, value in a_dictionary.items():
+            if value == max:
+                return key
