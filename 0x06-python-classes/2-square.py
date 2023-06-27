611d4c6151bb98905f91f39605a1a8ca2d22b03b
TeeMeeLehin <samemerald8@gmail.com>
2023-06-27 22:20:29 +0100

    Python OOP Class

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Instance of a square class"""
+
+
+class Square():
+    """Instance of a square class"""
+
+    def __init__(self, num = 0):
+        self.__size = num
+        if not isinstance(num, int):
+            raise TypeError("size must be an integer")
+        elif (num < 0):
+            raise ValueError("size must be >= 0")
