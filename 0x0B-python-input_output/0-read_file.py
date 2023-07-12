282eb491b58d9d60e5629e46013342c0cb8d0f62
TeeMeeLehin <samemerald8@gmail.com>
2023-07-12 13:01:52 +0100

    file i/o

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"function to read a file"
 
 def read_file(filename=""):
+    "printing file content to stdout"
     with open(filename) as f:
         for line in f:
             print(line, end="")
