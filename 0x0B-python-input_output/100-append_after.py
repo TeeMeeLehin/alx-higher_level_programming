5782ab6b00cb34c0de99dc973a596961698b80bb
TeeMeeLehin <samemerald8@gmail.com>
2023-07-12 15:03:58 +0100

    file i/o

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"appending text to file"
+
+
+def append_after(filename="", search_string="", new_string=""):
+    "function to append text to file  after a particular line"
+    with open(filename) as f:
+        for line in f:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as n:
+        n.write(text)
