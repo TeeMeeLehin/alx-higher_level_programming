bf7db9f6964aed8b833c79e26a645f6a62de7358
TeeMeeLehin <samemerald8@gmail.com>
2023-07-12 14:28:41 +0100

    file i/o

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"student class to json with filter"
+
+
+class Student():
+    """Instance of a square class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (attrs is None):
+            return self.__dict__
+        else:
+            return {attr: self.__dict__[attr] for attr
+                    in attrs if attr in self.__dict__}
