f31bddf1f43bdace391ded4c1296f98c6416eeee
TeeMeeLehin <samemerald8@gmail.com>
2023-07-17 20:51:28 +0100

    project tasks

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,6 +2,7 @@
 """test scipt for base class"""
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
 
 
 class TestBase(unittest.TestCase):
@@ -25,9 +26,34 @@ class TestBase(unittest.TestCase):
         self.assertEqual(base2.id, 200)
     
     def test_to_json(self):
-        pass
+        "testing the to_json method"
+        result = Base.to_json_string([])
+        self.assertEqual(result, "[]")
+        result = Base.to_json_string(None)
+        self.assertEqual(result, "[]")
+        data = [{"name": "John", "age": 30}, {"name": "Jane", "age": 25}]
+        result = Base.to_json_string(data)
+        self.assertEqual(result, '[{"name": "John", "age": 30}, {"name": "Jane", "age": 25}]')
 
     def test_save_to_file(self):
+        "testing the save_to_file function"
+        Rectangle.save_to_file([])
+        with open("Rectangle.json", "r") as file:
+            content = file.read()
+        self.assertEqual(content, "[]")
+        Rectangle.save_to_file(None)
+        with open("Rectangle.json", "r") as file:
+            content = file.read()
+        self.assertEqual(content, "[]")
+        class_instance_1 = Rectangle(2, 3)
+        class_instance_2 = Rectangle(3, 4)
+        class_instance_1.update(id=3)
+        class_instance_2.update(id=4)
+        Rectangle.save_to_file([class_instance_1, class_instance_2])
+        with open("Rectangle.json", "r") as file:
+            content = file.read()
+        expected = '[{"id": 3, "width": 2, "height": 3, "x": 0, "y": 0}, {"id": 4, "width": 3, "height": 4, "x": 0, "y": 0}]'
+        self.assertEqual(content, expected)
         pass
 
 if __name__ == '__main__':
