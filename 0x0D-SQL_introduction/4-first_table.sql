b494b3ae1bf4b70226ce567036a459d2d5052bfd
TeeMeeLehin <samemerald8@gmail.com>
2023-08-15 19:44:30 +0100

    sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
--- all tables on mySQL server
+-- create a table on mySQL server
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- create a table on mySQL server
+SHOW CREATE TABLE `first_table`;
