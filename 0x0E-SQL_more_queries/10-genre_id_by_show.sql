e0f8b390faf7eb2bc3714e377fb51f93631a25b1
TeeMeeLehin <samemerald8@gmail.com>
2023-08-16 22:29:34 +0100

    sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,3 +1,3 @@
 -- listing records from an imported DB
 USE hbtn_0d_tvshows
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
