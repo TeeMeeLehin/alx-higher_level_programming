22728a028c1c2e9a8c54d09a8a0da89ecb3b1d4d
TeeMeeLehin <samemerald8@gmail.com>
2023-08-16 23:49:51 +0100

    sql

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,8 @@
+-- sorting records by their ratings
+SELECT title, SUM(tv_show_ratings.rate) AS 'rating'
+FROM tv_shows
+LEFT JOIN tv_show_ratings
+ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY title
+ORDER BY rating DESC
+;
