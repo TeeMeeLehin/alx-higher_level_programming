04eb393221fe08756ba4f3ec12230e35093a21ba
TeeMeeLehin <samemerald8@gmail.com>
2023-08-16 23:39:00 +0100

    sql

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,12 @@
+-- queryin DB for record information
+SELECT tv_genres.name FROM tv_genres
+WHERE tv_genres.name NOT IN
+(
+SELECT tv_genres.name FROM tv_shows INNER JOIN tv_show_genres 
+ON tv_shows.id = tv_show_genres.show_id 
+INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name
+)
+ORDER BY tv_genres.name
+;
