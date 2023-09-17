504ba576ff8656fe4cc2d9bd1f83b8d9683bcda7
TeeMeeLehin <samemerald8@gmail.com>
2023-09-17 16:09:11 +0100

    Py ORM

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""script that lists one State object from the database """
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], "", sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    state = session.query(State).filter(State.name.like('{}'.format(sys.argv[4]))).first()
+    if state is not None:
+        print("{}".format(state.id))
+    else:
+        print("Not found")
+    session.close()
