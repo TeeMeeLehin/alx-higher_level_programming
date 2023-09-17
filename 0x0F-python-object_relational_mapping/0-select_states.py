73adf8aa6ea56e528a77746f4013aa3f16b37f34
TeeMeeLehin <samemerald8@gmail.com>
2023-09-17 12:07:26 +0100

    Py ORM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd="", db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states ORDER BY states.id")
     rows = cur.fetchall()
     for row in rows:
         print("%s" % str(row))
---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd="", db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    qry = "SELECT * FROM states WHERE name \
+    LIKE BINARY %s ORDER BY states.id", (sys.argv[4])
+    cur.execute(qry)
+    rows = cur.fetchall()
+    for row in rows:
+        print("%s" % str(row))
+
+    cur.close()
+    db.close()
