200fc336289717621456531b373f722288f37b25
TeeMeeLehin <samemerald8@gmail.com>
2023-09-17 15:55:43 +0100

    Py ORM

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""script that lists one State objects from the database """
+"""script that lists one State object from the database """
 import sys
 from model_state import Base, State
 from sqlalchemy.orm import Session
@@ -8,10 +8,10 @@ from sqlalchemy import create_engine
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           .format(sys.argv[1], "", sys.argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = Session(engine)
-    state = session.query(State).order_by(State.id).one()
+    state = session.query(State).order_by(State.id).first()
     print("{}: {}".format(state.id, state.name))
     session.close()
