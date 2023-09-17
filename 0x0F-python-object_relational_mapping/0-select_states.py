d4cb34ab3e7814e3cdfbe90bd972d5c183572793
TeeMeeLehin <samemerald8@gmail.com>
2023-09-17 11:48:11 +0100

    Py ORM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """  lists all states from the database hbtn_0e_0_usa """
+
 import MySQLdb
 import sys
 
@@ -11,3 +12,6 @@ if __name__ == "__main__":
     rows = cur.fetchall()
     for row in rows:
         print("%s" % str(row))
+
+    cur.close()
+    db.close()
