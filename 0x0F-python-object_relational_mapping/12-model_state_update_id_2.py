a294ef04684ef1e189b1888fa15a9e84397062a8
TeeMeeLehin <samemerald8@gmail.com>
2023-09-17 16:24:12 +0100

    Py ORM

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
     session = Session(engine)
-    s = session.query(State).filter(State.id==2).first()
+    s = session.query(State).filter(State.id == 2).first()
     s.name = "New Mexico"
     session.commit()
     session.close()
