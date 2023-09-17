b148708939401d087ab1ca338ea6ebbd9393bda7
TeeMeeLehin <samemerald8@gmail.com>
2023-09-17 11:53:18 +0100

    Py ORM

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+"""  lists all states from the database hbtn_0e_0_usa starting with N"""
 
 import MySQLdb
 import sys
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd="", db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id")
+    qry = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id"
+    cur.execute(qry)
     rows = cur.fetchall()
     for row in rows:
         print("%s" % str(row))
