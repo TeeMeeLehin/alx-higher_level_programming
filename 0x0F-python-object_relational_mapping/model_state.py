9dae77928f688ba8bce1975cfc57b1103638243e
TeeMeeLehin <samemerald8@gmail.com>
2023-09-17 13:10:40 +0100

    Py ORM

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" defining a class object to link with ORM """
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """ defining the state class """
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
