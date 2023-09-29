5fbc323f6659a5f353797534c65f350027a5e539
TeeMeeLehin <samemerald8@gmail.com>
2023-09-29 16:03:40 +0100

    urllib error

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+ Python script that takes in a URL,
+ sends a request to the URL
+ and displays the body of the response
+"""
+
+import sys
+import urllib.request
+from urllib.error import HTTPError
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    with urllib.request.urlopen(url) as response:
+        try:
+            print(response.read().decode('utf-8'))
+        except HTTPError as e:
+            print(f"Error code: {e.code}")
