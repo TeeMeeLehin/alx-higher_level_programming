a3aa22965c7275e8051f6a8ec203e506e0a5b9b7
TeeMeeLehin <samemerald8@gmail.com>
2023-09-29 17:34:25 +0100

    requests json

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+ Python script that takes in a letter and
+ sends a POST request to http://0.0.0.0:5000/search_user
+ with the letter as a parameter.
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+
+    if len(sys.argv) == 1:
+        data = {'q': ""}
+    else:
+        data = {'q': sys.argv[1]}
+
+    response = requests.post("http://0.0.0.0:5000/search_user", data=data)
+
+    try:
+        response = response.json()
+        if response == {}:
+            print("No result")
+        else:
+            print(f"[{response.get('id')}] {response.get('name')}")
+
+    except ValueError:
+        print("Not a valid JSON")
