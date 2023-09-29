a33e7e9d280ed2d8fdca364bd7b6aebd6130b394
TeeMeeLehin <samemerald8@gmail.com>
2023-09-29 14:49:45 +0100

    urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""  Python script that fetches https://alx-intranet.hbtn.io/status """
+
+import urllib.request
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    response = response.read()
+    print("Body response:")
+    print(f"\t- type: {type(response)}")
+    print(f"\t- content: {response}")
+    print(f"\t- utf8 content: {response.decode('utf-8')}")
