aacebabff55be87dd4feba26698a5177ff4d653e
TeeMeeLehin <samemerald8@gmail.com>
2023-09-29 20:13:49 +0100

    retrieving commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Python script to solve ALXSE technical challenge
+The script displays the last 10 github commtis
+of a given repo.
+"""
+
+import sys
+import requests
+
+url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
+response = requests.get(url)
+response = response.json()
+
+for item in response[:10]:
+    sha = item.get("sha")
+    author = item.get("commit").get("author").get("name")
+    print(f"{sha}: {author}")
