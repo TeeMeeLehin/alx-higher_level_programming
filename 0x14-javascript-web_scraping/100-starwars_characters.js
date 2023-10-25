a8e856c57c2d041b0c45aeddb5b1a3db621f502b
TeeMeeLehin <samemerald8@gmail.com>
2023-10-25 19:49:15 +0100

    js web scraping

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request(url, (err, response) => {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const stars = JSON.parse(response.body).characters;
+    for (const star of stars) {
+      request(star, (err, response) => {
+        if (err) {
+          console.log(err);
+        } else if (response.statusCode === 200) {
+          console.log(JSON.parse(response.body).name);
+        } else {
+          console.log(`code: ${response.statusCode}`);
+        }
+      });
+    }
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
